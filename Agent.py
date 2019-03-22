ec27a26827557a14394491763abde23460f5a0a2
Sparsha Saha <saha.sparsha@gmail.com>
2019-03-22 16:35:54 +0530

    Agent added

---
+++ Agent.py
@@ -0,0 +1,24 @@
+import neat
+
+class Agent(neat.DefaultGenome):
+
+    def __init__(self, key):
+        super().__init__(key)
+
+        # Define score
+        self.score = 0
+
+    def configure_new(self, config):
+        super().configure_new(config)
+    
+
+    def configure_crossover(self, genome1, genome2, config):
+        super().configure_crossover(genome1, genome2, config)
+    
+
+    def mutate(self, config):
+        super().mutate(config)
+    
+
+    def distance(self, other, config):
+        return super().distance(other, config)
